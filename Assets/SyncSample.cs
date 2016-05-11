089cf88021c9b8f628b8cbbe239ac883d8211149
Masayoshi Nakamura <mchmasa@gmail.com>
2016-05-11 18:20:52 +0900

    first commit

---
+++ Assets/SyncSample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncSample : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject prefab;
+	int targetFps = 10;
+
+	[Range(0.1f, 100f)]
+	public float timeScale = 1;
+
+	void Start () {
+	}
+
+	int cnt = 0;
+	void Update () {
+		QualitySettings.vSyncCount = 0;
+		float deltaTime = 1.0f / (float)targetFps;
+		Time.timeScale = timeScale;
+		Time.captureFramerate = (int)(1.0f / deltaTime * timeScale);
+		//Application.targetFrameRate = 300;
+
+		cnt++;
+		if (cnt % targetFps == 0) {
+			Object.Instantiate(prefab, new Vector3(0,5,0), Quaternion.identity);
+			Debug.Log ((cnt/targetFps).ToString() + "sec passed.");
+		}
+	}
+}
